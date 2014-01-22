dcfef5ecf7ac622c3359a65e0a9aaa695120d5dc
Gavin Hales <gavinahales@gmail.com>
2014-01-22 16:39:20 +0000

    SQL code added to Autopsy processor

--- InsightPreprocessor/Program.cs
+++ InsightPreprocessor/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.SQLite;
+using System.Xml;
 
 namespace InsightPreprocessor
 {
@@ -11,6 +13,7 @@ namespace InsightPreprocessor
     {
 
         static String projpath;
+        static XmlDocument xmldoc;
 
         static void Main(string[] args)
         {
@@ -38,6 +41,8 @@ namespace InsightPreprocessor
                             Console.ForegroundColor = ConsoleColor.DarkGreen;
                             Console.WriteLine("Autopsy file found! Processing...");
                             Console.ForegroundColor = ConsoleColor.White;
+
+                            xmldoc = new XmlDocument();
 
                             //Begin processing autopsy file.
                             bool autopsysuccess = ProcessAutopsy(file.FullName);
@@ -81,12 +86,35 @@ namespace InsightPreprocessor
                 Console.WriteLine("FAILED: Directory not found. Error: "+e.Message);
             }
 
-            
 
+            Console.ReadLine();
         }
 
         public static bool ProcessAutopsy(String path)
         {
+
+            SQLiteConnection sqldb = new SQLiteConnection("Data Source="+path+";Version=3;Read Only=True;");
+            sqldb.Open();
+
+            #region Get Web History
+            SQLiteCommand getweb = new SQLiteCommand(sqldb);
+            getweb.CommandText = "SELECT DISTINCT artifact_id FROM blackboard_attributes WHERE artifact_id IN (SELECT artifact_id FROM blackboard_artifacts WHERE artifact_type_id = 4) AND attribute_type_id = 33";
+            SQLiteDataReader webresults = getweb.ExecuteReader();
+
+            //Loop through each web history artifact
+            while (webresults.Read())
+            {
+
+                SQLiteCommand getdate = new SQLiteCommand(sqldb);
+                getdate.CommandText = "SELECT value_int64 FROM blackboard_attributes WHERE (artifact_id = " + webresults.GetValue(0).ToString() + " AND attribute_type_id = 33)";
+                String date = getdate.ExecuteScalar().ToString();
+
+                Console.WriteLine(date);
+
+            }
+
+            #endregion
+
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Autopsy processing not yet implemented.");
             Console.ForegroundColor = ConsoleColor.White;
@@ -96,7 +124,7 @@ namespace InsightPreprocessor
         public static bool ProcessL2T(String path)
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Autopsy processing not yet implemented.");
+            Console.WriteLine("Log2Timeline processing not yet implemented.");
             Console.ForegroundColor = ConsoleColor.White;
             return false;
         }
